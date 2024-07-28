1bcf88058d4cbb9024c113d78d845e1d3bbb4060
satish <satishhargod@gmail.com>
2024-07-28 21:34:30 +0530

    new data

--- practice.js
+++ practice.js
@@ -1,14 +1,18 @@
-const data = [4,-5,3,6,8]
-let value = 0
-function squears(arr, length){
-    if(length){
-        if(arr[length-1] > 0){
-            squear = arr[length-1] * arr[length-1]
-            value += squear
-        }        
-        length = length-1;
-        squears(data, length)
-    }
-}
+// const data = [4,-5,3,6,8]
+// let value = 0
+// function squears(arr, length){
+//     if(length){
+//         if(arr[length-1] > 0){
+//             squear = arr[length-1] * arr[length-1]
+//             value += squear
+//         }        
+//         length = length-1;
+//         squears(data, length)
+//     }
+// }
 
-squears(data, data.length)
+// squears(data, data.length)
+let data = [45, 4, 5, 6, 7];
+
+console.log(data.sort((a, b)=> b-a))
+
